e37f82be7295e3ec47972388ee93ea3b9e081905
dasterisk <sleeper12312@gmail.com>
2018-10-10 12:23:21 +0800

    filtered ls(5of13)

--- program.js
+++ program.js
@@ -1,14 +1,16 @@
 var fs = require('fs');
-var file = process.argv;
-console.log(file);
-fs.readdir(file[2], function(err, list){
+var path = require('path');
+
+var folder = process.argv[2];
+var ext = '.' + process.argv[3];
+
+fs.readdir(folder, function(err, files){
 	if(err){
 		console.log(err);
 	}
-	for(var i = 0; i < list.length; i++){
-		console.log(list);
-		if (list[i].extname() == 'txt') {
-			console.log(list[i]);
+	files.forEach(function(file){
+		if (path.extname(file)== ext) {
+			console.log(file);
 		}
-	}
+	})
 });
